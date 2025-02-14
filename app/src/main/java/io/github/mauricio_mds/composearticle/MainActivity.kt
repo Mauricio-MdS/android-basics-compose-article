82394e8feca11d26d8b51924f52d376a1285a51a
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2025-02-14 19:00:30 -0300

    Implements Article

--- app/src/main/java/io/github/mauricio_mds/composearticle/MainActivity.kt
+++ app/src/main/java/io/github/mauricio_mds/composearticle/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.mauricio_mds.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeArticleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Article(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Article(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Image(painterResource(R.drawable.bg_compose_background), contentDescription = null)
+        Text(
+            text = stringResource(R.string.tutorial_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.first_paragraph),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+        )
+        Text(
+            text = stringResource(R.string.second_paragraph),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp),
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        Article()
     }
 }
